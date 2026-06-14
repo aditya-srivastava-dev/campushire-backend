b93560bed5d1893739c276f9d34dcf35262cfec9
Aditya Srivastava <adi7905993787@gmail.com>
2026-06-14 14:18:31 +0530

    Setup backend and MongoDB connection

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dns").setDefaultResultOrder("ipv4first");
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("CampusHire Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
